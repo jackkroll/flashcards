643c7a0d92d33e5628d5cefa2460ebe00b93f7b8
jack <52892975+jackkroll@users.noreply.github.com>
2025-11-25 15:14:31 -0500

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,32 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Jack Kroll on 11/25/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct todoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
